ad77d6bdf6e5f72efea77c91b3b962b5a1216ca0
cenkgurbet <cenk.gurbet@deepnetwork.com>
2023-08-01 14:05:44 +0200

    CG - Add unit tests. - 1

--- DemoProject/tests/unit.py
+++ DemoProject/tests/unit.py
@@ -1,10 +1,12 @@
 import unittest
-from utils import get_daily_data
+from utils import get_daily_data, get_weekly_data
 
 class UnitTest(unittest.TestCase):
-    def testApiCall(self):
+    def testApiCallDailyData(self):
         self.assertIsNotNone(get_daily_data(),"Api call return no values")
 
+    def testApiCallWeeklyData(self):
+        self.assertIsNotNone(get_weekly_data(),"Api call return no values")
    
 if __name__ == '__main__':
    unittest.main()
