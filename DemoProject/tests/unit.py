d274fff248626596200e2240a7aa1b93c1019b53
cenkgurbet <cenk.gurbet@deepnetwork.com>
2023-08-01 16:26:28 +0200

    CG - Test unit tests in GitHub Actions. - 1

--- DemoProject/tests/unit.py
+++ DemoProject/tests/unit.py
@@ -1,4 +1,10 @@
 import unittest
+import sys
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
 from utils import get_daily_data, get_weekly_data
 
 class UnitTest(unittest.TestCase):
